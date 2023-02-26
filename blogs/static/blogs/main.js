226b8485e989f1d804863bca1b43ebaa40521da2
shinhosuck <shinhosuck1973@gmail.com>
2023-02-26 09:01:00 +0800

    update

--- blogs/static/blogs/main.js
+++ blogs/static/blogs/main.js
@@ -1,1 +1,26 @@
+// NAV
+const navItems = document.querySelector('.nav-items')
+const navItemsOpenBtn = document.querySelector('.nav-items-open-btn')
+const navItemsCloseBtn = document.querySelector('.nav-items-close-btn')
 
+
+
+const handleEvents = function(event){
+    if(event.currentTarget == navItemsOpenBtn){
+        navItems.classList.add("show-nav-items")
+        navItemsOpenBtn.style.display = "none"
+        navItemsCloseBtn.style.display = "inline-block"
+    }
+    if(event.currentTarget == navItemsCloseBtn){
+        navItems.classList.remove("show-nav-items")
+        navItemsOpenBtn.style.display = "inline-block"
+        navItemsCloseBtn.style.display = "none"
+    }
+}
+
+
+
+
+
+navItemsOpenBtn.addEventListener('click', handleEvents)
+navItemsCloseBtn.addEventListener('click', handleEvents)
